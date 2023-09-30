03fc6e968ff95701bdd019fda49317ab09425194
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-30 20:24:24 +0800

    test: add user list test

---
+++ src/UserList.test.js
@@ -0,0 +1,43 @@
+import { render, screen, within } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserList from './UserList';
+
+test('it renders one row per user', () => {
+  // render component
+  const users = [
+    { name: 'binta', email: 'binta@mail.com' },
+    { name: 'dave', email: 'dave@mail.com' },
+  ];
+
+  const { container } = render(<UserList users={users} />);
+
+  // find all the rows in the table
+
+  // Method 1:
+  // eslint-disable-next-line
+  // const rows = container.querySelectorAll('tbody tr'); // ignore warning
+
+  // Method 2: assuming ' data-testid="users" ' attribute has been added to the html element (tbody)
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+  // assert correct number of rows in the table
+  expect(rows).toHaveLength(2);
+});
+
+test('it render the email and name of each user', () => {
+  // render component
+  const users = [
+    { name: 'binta', email: 'binta@mail.com' },
+    { name: 'dave', email: 'dave@mail.com' },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (const user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});

006a0120f712a7bff41a0220419e7cefe3b8b046
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-30 20:50:59 +0800

    test: add test for the entire App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('it can recieve a new user and show it on a list', () => {
+  render(<App />);
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  user.click(nameInput);
+  user.keyboard('mark');
+  user.click(emailInput);
+  user.keyboard('mark@hotmail.com');
+
+  user.click(button);
+
+  // screen.debug();
+
+  const name = screen.getByRole('cell', { name: 'mark' });
+  const email = screen.getByRole('cell', { name: 'mark@hotmail.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
